f34328d2194866ad26a4aceac709435814361614
WEBDEV-Rajat <162951475+WEBDEV-Rajat@users.noreply.github.com>
2026-05-27 13:01:36 +0530

    added ui for homepage

---
+++ components/HomeCard.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Image from 'next/image'
+
+interface HomeCardProps {
+  className: string
+  img: string
+  title: string
+  description: string
+  handleClick: () => void
+}
+
+const HomeCard = ({
+  className,
+  img,
+  title,
+  description,
+  handleClick,
+}: HomeCardProps) => {
+  return (
+    <div
+      className={`px-4 py-6 flex flex-col justify-between w-full xl:max-w-67.5
+      min-h-65 rounded-[14px] cursor-pointer ${className}`}
+      onClick={handleClick}
+    >
+      <div className='flex-center glassmorphism size-12 rounded-[10px]'>
+        <Image src={img} alt='meeting' width={27} height={27} />
+      </div>
+
+      <div className='flex flex-col gap-2'>
+        <h1 className='text-2xl font-bold'>{title}</h1>
+        <p className='text-lg font-normal'>{description}</p>
+      </div>
+    </div>
+  )
+}
+
+export default HomeCard
